40bcc1fe3a236a5b59ee2471f0ffed6dc4525736
Petr Penzin <penzin.dev@gmail.com>
2017-10-24 18:59:20 -0700

    Version 2.0

--- setup.py
+++ setup.py
@@ -1,44 +1,74 @@
+import fileinput, os, sys
 from distutils.core import Extension, setup
 from distutils import sysconfig
+
+xft         = []
+cargs       = []
+largs       = []
+install_dir = sysconfig.get_python_lib() + "/pypanel"
+files       = ["COPYING", "README", "ppicon.png", "pypanelrc"]
 
 try:
     from Xlib import X, display, Xatom, Xutil
 except:
     print "\nPyPanel requires the Python X library -"
     print "http://sourceforge.net/projects/python-xlib/"
-    import sys
     sys.exit()
-#----------------------------------------------------------------------------
-# Building PyPanel with XPM support will allow you to use a default icon
-# (src/ppicon.xpm) for applications which don't provide one (e.g. Xterm).
-#
-# Adjust the include_dirs and library_dirs below to suit your system.  To 
-# build without Xft or Xpm support, remove the appropriate string from
-# 'libraries' and the appropriate tuple from 'define_macros'.
-#----------------------------------------------------------------------------
-module = Extension(
-    "ppmodule",
-    sources       = ["src/ppmodule.c"],
-    include_dirs  = ["/usr/X11R6/include",
-                     "/usr/include/freetype2",],
-    library_dirs  = ["/usr/X11R6/lib"],
-    libraries     = ["X11", "Xft", "Xpm"],  
-    define_macros = [("HAVE_XFT", 1), ("HAVE_XPM", 1)],                    
-    )
 
-install_dir = sysconfig.get_python_lib() + "/pypanel"
-files       = ["COPYING", "README", "pypanelrc"]
-script      = "src/pypanel"
 
-setup (name             = "PyPanel",
-       author           = "Jon Gelo",
-       author_email     = "ziljian@users.sourceforge.net",
-       version          = "1.3",
-       license          = "GPL",
-       platforms        = "POSIX",
-       description      = "Lightweight panel/taskbar for X11 Window Managers",
-       long_description = "See README for more information",
-       url              = "http://pypanel.sourceforge.net",
-       data_files       = [(install_dir, files)],
-       scripts          = [script],
-       ext_modules      = [module])
+for config in ("freetype-config", "imlib2-config"):
+    cflags = os.popen("%s --cflags" % config).read().strip().split()
+    if cflags:
+        for cflag in cflags:
+            if cflag not in cargs:
+                cargs.append(cflag)
+    
+        if config == "freetype-config":
+            xft.append(("HAVE_XFT",1))
+            largs.append("-lXft")
+           
+        libs = os.popen("%s --libs" % config).read().strip().split()
+        if libs:
+            for lib in libs:
+                if lib not in largs:
+                    largs.append(lib)
+    else:
+        if config == "imlib2-config":
+            print "\nPyPanel requires the Imlib2 library -"
+            print "http://www.enlightenment.org/pages/imlib2.html"
+            sys.exit()
+    
+cargs.sort()
+largs.sort()
+
+# Fix the shebang
+for line in fileinput.input(["pypanel"], inplace=1):
+    if fileinput.isfirstline():
+        print "#!%s -O" % sys.executable
+    else:
+        print line,        
+
+module = Extension("ppmodule",
+                   sources            = ["ppmodule.c"],
+                   include_dirs       = ["/usr/X11R6/include"],
+                   library_dirs       = [],
+                   libraries          = [],  
+                   extra_compile_args = cargs,
+                   extra_link_args    = largs,
+                   define_macros      = xft,                    
+                  )  
+
+setup(name             = "PyPanel",
+      author           = "Jon Gelo",
+      author_email     = "ziljian@users.sourceforge.net",
+      version          = "2.0",
+      license          = "GPL",
+      platforms        = "POSIX",
+      description      = "Lightweight panel/taskbar for X11 Window Managers",
+      long_description = "See README for more information",
+      url              = "http://pypanel.sourceforge.net",
+      data_files       = [(install_dir, files)],
+      scripts          = ["pypanel"],
+      ext_modules      = [module]
+     )
+     
